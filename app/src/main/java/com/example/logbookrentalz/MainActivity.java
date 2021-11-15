27ac9f8eac06b38791dc1970afc740912e01590b
Your Name <quytvgcd17298@fpt.edu.vn>
2021-11-15 19:28:34 +0700

    Complete validation input

--- app/src/main/java/com/example/logbookrentalz/MainActivity.java
+++ app/src/main/java/com/example/logbookrentalz/MainActivity.java
@@ -2,23 +2,35 @@ package com.example.logbookrentalz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     EditText property, datetime, price, note, reporter;
     Spinner spinnerBedroom, spinnerFurniture;
     Button submit;
     SQLite db;
+    DatePickerDialog.OnDateSetListener setListener;
 
+    AwesomeValidation awesomeValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +45,17 @@ public class MainActivity extends AppCompatActivity {
         submit = findViewById(R.id.submit);
 
         db = new SQLite(this);
+
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        awesomeValidation.addValidation(this,R.id.property,
+                RegexTemplate.NOT_EMPTY,R.string.invalid_property);
+        awesomeValidation.addValidation(this,R.id.datetime,
+                RegexTemplate.NOT_EMPTY,R.string.invalid_datetime);
+        awesomeValidation.addValidation(this,R.id.price,
+                RegexTemplate.NOT_EMPTY,R.string.invalid_price);
+        awesomeValidation.addValidation(this,R.id.reporter,
+                RegexTemplate.NOT_EMPTY,R.string.invalid_reporter);
 
         spinnerBedroom = (Spinner)findViewById(R.id.spinnerBedroom);
 
@@ -66,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         spinnerFurniture = (Spinner)findViewById(R.id.spinnerFurniture);
 
         final ArrayList<String> arrayFurniture = new ArrayList<String>();
-        arrayFurniture.add("Furniture Types");
+        arrayFurniture.add(0,"Furniture Types");
         arrayFurniture.add("Classic");
         arrayFurniture.add("NeoClassic");
         arrayFurniture.add("Modern");
@@ -80,7 +103,16 @@ public class MainActivity extends AppCompatActivity {
         spinnerFurniture.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            if (adapterView.getItemAtPosition(i).equals("Furniture Types"))
+            {
+
+                Toast.makeText(MainActivity.this, "Please enter furniture types !!!", Toast.LENGTH_LONG).show();
+
+            }
+            else
+            {
                 Toast.makeText(MainActivity.this, arrayFurniture.get(i), Toast.LENGTH_SHORT).show();
+            }
             }
 
             @Override
@@ -89,24 +121,65 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        datetime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        month = month +1;
+                        String date = day+"/"+month+"/"+year;
+                        datetime.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
+
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String propertyText = property.getText().toString();
-                String bedroomText = spinnerBedroom.toString();
+                String bedroomText = spinnerBedroom.getSelectedItem().toString();
                 String datetimeText = datetime.getText().toString();
                 String priceText = price.getText().toString();
-                String furnitureText = spinnerFurniture.toString();
+                String furnitureText = spinnerFurniture.getSelectedItem().toString();
                 String noteText = note.getText().toString();
                 String reporterText = reporter.getText().toString();
 
+                Cursor cursor = db.getData();
 
-                if (!propertyText.isEmpty() & !bedroomText.isEmpty() & !datetimeText.isEmpty() &
-                    !priceText.isEmpty() & !furnitureText.isEmpty() & !reporterText.isEmpty()){
+                if (awesomeValidation.validate() && !bedroomText.equals("Bedrooms") && !furnitureText.equals("Furniture Types")){
                     Boolean insertedDataSuccessful = db.insertAndroidData(propertyText, bedroomText, datetimeText,priceText,furnitureText,noteText,reporterText);
-                    if(insertedDataSuccessful) {
-                        Toast.makeText(MainActivity.this, "Inserted Successfully !!!", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Insert Completed", Toast.LENGTH_LONG).show();
+
+                    if(cursor.getCount() == 0){
+                        Toast.makeText(MainActivity.this, "NO ENTRY", Toast.LENGTH_LONG
+                        ).show();
+                    }
+                    else{
+                        StringBuffer buffer = new StringBuffer();
+                        while(cursor.moveToNext()){
+                            buffer.append("Property Type: " + cursor.getString(0) + "\n");
+                            buffer.append("Bedroom: " + cursor.getString(1) + "\n");
+                            buffer.append("Date: " + cursor.getString(2) + "\n");
+                            buffer.append("Monthly Price: " + cursor.getString(3) + "\n");
+                            buffer.append("Furniture Type: " + cursor.getString(4) + "\n");
+                            buffer.append("Note: " + cursor.getString(5) + "\n");
+                            buffer.append("Reporter Name: " + cursor.getString(6) + "\n\n\n");
+                        }
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setCancelable(true);
+                        builder.setTitle("Input Details");
+                        builder.setMessage(buffer.toString());
+                        builder.show();
                     }
                 }
                 else{
--- app/src/main/java/com/example/logbookrentalz/SQLite.java
+++ app/src/main/java/com/example/logbookrentalz/SQLite.java
@@ -2,6 +2,7 @@ package com.example.logbookrentalz;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -21,7 +22,7 @@ public class SQLite extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS AndroidDetails");
     }
 
-    public Boolean insertAndroidData(String property, String bedroom, String datetime, String price, String furniture, String note, String reporter){
+    public Boolean insertAndroidData(String property, String bedroom, String datetime, String price, String furniture, String note, String reporter) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("property", property);
@@ -33,5 +34,10 @@ public class SQLite extends SQLiteOpenHelper {
         contentValues.put("reporter", reporter);
         long result = db.insert("AndroidDetails", null, contentValues);
         return result != 1;
+    }
+        public Cursor getData(){
+            SQLiteDatabase DB = this.getReadableDatabase();
+            Cursor cursor = DB.rawQuery("SELECT * FROM AndroidDetails", null);
+            return cursor;
     }
 }
